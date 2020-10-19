599180a123b3004a6d8a3cefe92ae95fc5586d09
Carrie <carrieboardman92@hotmail.com>
2020-10-19 22:47:07 +0100

    separate map into own component

---
+++ wl-litter-picking/src/Map/MapContainer.jsx
@@ -0,0 +1,108 @@
+import React, {useEffect, useRef} from 'react';
+import Feature from 'ol/Feature';
+import { Point } from 'ol/geom';
+import { Style, Text, Stroke, Fill } from 'ol/style';
+import Draw from 'ol/interaction/Draw';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import {Tile, Vector as VectorLayer} from 'ol/layer';
+import { XYZ, Vector} from 'ol/source';
+import {transform} from 'ol/proj.js';
+
+import 'ol/ol.css';
+
+const MapContainer = props => {
+
+    const { drawingEnabled, addMarkerEnabled, setDrawingEnabled, setAddMarkerEnabled } = props;
+
+    const map = useRef();
+
+    useEffect(()=>{
+      
+      const vectorLayer = new VectorLayer({source: new Vector()});
+  
+      const drawingLayer = new VectorLayer({
+          source: new Vector({    
+            }),
+            style: new Style({
+              fill: new Fill({
+                color: 'rgba(225, 225, 225, 0.4)',
+              }),
+              stroke: new Stroke({
+                width: 2,
+                color: 'blue'
+              })
+          })
+      });
+  
+      const tileLayer = new Tile({
+        source: new XYZ({
+          url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+          attributions: '© OpenStreetMap contributors'
+        })
+      });
+  
+      const drawingInteraction =new Draw({
+        type: 'Polygon',
+        source: drawingLayer.getSource(),
+      });
+    
+      map.current = new Map({
+        target: 'mapid',
+        layers: [tileLayer, vectorLayer, drawingLayer],
+        view: new View({
+          center: transform([ '-3.553118', '55.9124'], 'EPSG:4326', 'EPSG:3857'),
+          zoom: 12
+        }),
+      });
+      
+      map.current.addInteraction(drawingInteraction);
+      
+    },[]);
+
+    const fn = useRef(event => {
+      var iconFeature = new Feature({
+        geometry: new Point(event.coordinate)
+    })
+  
+    var iconStyle = new Style({
+      text: new Text({
+        text: 'room',                    
+        font: '900 30px "Material Icons"',
+        color: '#008000' 
+      })
+    }); 
+  
+    iconFeature.setStyle(iconStyle);
+    map.current.getLayers().item(1).getSource().addFeature(iconFeature);
+    });
+  
+    useEffect(()=>{
+      if (addMarkerEnabled) {
+        if(drawingEnabled) setDrawingEnabled(false);
+        map.current.on('singleclick', fn.current );
+      } else {
+        map.current.un('singleclick', fn.current);
+      }
+    }, [addMarkerEnabled, drawingEnabled, setDrawingEnabled])
+  
+    useEffect(()=> {
+      if(map.current) {
+     
+        if (addMarkerEnabled && drawingEnabled) setAddMarkerEnabled(false);
+        map.current.getInteractions().forEach(interaction => {
+          if(interaction instanceof Draw){
+            if (drawingEnabled) interaction.setActive(true);
+            else interaction.setActive(false);
+          } 
+        });
+      }
+    }, [drawingEnabled, addMarkerEnabled, setAddMarkerEnabled])
+  
+
+    return (
+        <div id="mapid" />
+    )
+}
+
+export default MapContainer;
